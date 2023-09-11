ee211bf3ff214b13f33ea05f845536b84e44ded1
MRAlirad <m.r.alirad75@gmail.com>
2023-09-11 18:46:29 +0330

    redirecting and catch not found pages

--- src/main.js
+++ src/main.js
@@ -6,13 +6,18 @@ import App from './App.vue';
 import UserList from './projects/users-and-teams/UserList.vue';
 import TeamsList from './projects/users-and-teams/TeamsList.vue';
 import TeamMembers from './projects/users-and-teams/TeamMembers.vue';
+import NotFound from './projects/users-and-teams/NotFound.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        {path: '/teams', component: TeamsList},
+        {path: '/', redirect: '/teams'},
+        {path: '/teams', component: TeamsList },
+        // {path: '/teams', component: TeamsList , alias: '/'},
         {path: '/users', component: UserList},
         {path: '/teams/:teamId', component: TeamMembers, props: true},
+        // {path: '/:notFound(.*)', redirect: '/teams'},
+        {path: '/:notFound(.*)', component: NotFound},
     ],
 });
 
