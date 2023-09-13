7a23ffd05c950cce2500624afaa36b615a883b4e
MRAlirad <m.r.alirad75@gmail.com>
2023-09-13 19:30:38 +0330

    saved position

--- src/main.js
+++ src/main.js
@@ -26,7 +26,7 @@ const router = createRouter({
                     name: 'team-members',
                     path: ':teamId',
                     component: TeamMembers,
-                    props: true
+                    props: true,
                 },
             ],
         },
@@ -41,6 +41,12 @@ const router = createRouter({
         // {path: '/:notFound(.*)', redirect: '/teams'},
         {path: '/:notFound(.*)', component: NotFound},
     ],
+    scrollBehavior(to, from, savedPosition)
+    {
+        if (savedPosition)
+            return savedPosition;
+        return {left: 0, top: 0};
+    }
 });
 
 createApp(App).use(router).mount('#app');
