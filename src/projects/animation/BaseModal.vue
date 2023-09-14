e808538ba916a41fafdee05b6d81c408d2fe34fd
MRAlirad <m.r.alirad75@gmail.com>
2023-09-14 10:50:54 +0330

    modal opening animaiton

--- src/projects/animation/BaseModal.vue
+++ src/projects/animation/BaseModal.vue
@@ -37,5 +37,18 @@
 		background-color: white;
 		z-index: 100;
 		border: none;
+		animation: modal 0.3s ease-out;
+	}
+
+	@keyframes modal {
+		from {
+			opacity: 0;
+			transform: translateY(-50px) scale(0.9);
+		}
+
+		to {
+			opacity: 1;
+			transform: translateY(0) scale(1);
+		}
 	}
 </style>
