202bafef19955e5884a5fb580379ffbff8fe9fe8
MRAlirad <m.r.alirad75@gmail.com>
2023-09-14 10:35:24 +0330

    basic animation

--- src/App.vue
+++ src/App.vue
@@ -5,28 +5,31 @@
 	<!-- <LearningResourcesApp /> -->
 	<!-- <Form /> -->
 	<!-- <SurvayResultsApp /> -->
-	<UsersTeamsApp />
+	<!-- <UsersTeamsApp /> -->
+	<Animation />
 </template>
 
 <script>
-	import MyGoals from './projects/addGoals/AddGoals.vue';
-	import MonsterSlayer from './projects/monsterSlayer/MonsterSlayer.vue';
-	import FriendsContact from './projects/friends-contact-info/FriendsContact.vue';
-	import LearningResourcesApp from './projects/learning-resources/LearningResourcesApp.vue';
-	import Form from './projects/form/Form.vue';
-	import SurvayResultsApp from './projects/survay-results/SurvayResultsApp.vue';
-	import UsersTeamsApp from './projects/users-and-teams/UsersTeamsApp.vue';
+	// import MyGoals from './projects/addGoals/AddGoals.vue';
+	// import MonsterSlayer from './projects/monsterSlayer/MonsterSlayer.vue';
+	// import FriendsContact from './projects/friends-contact-info/FriendsContact.vue';
+	// import LearningResourcesApp from './projects/learning-resources/LearningResourcesApp.vue';
+	// import Form from './projects/form/Form.vue';
+	// import SurvayResultsApp from './projects/survay-results/SurvayResultsApp.vue';
+	// import UsersTeamsApp from './projects/users-and-teams/UsersTeamsApp.vue';
+	import Animation from './projects/animation/Animation.vue';
 
 	export default {
 		name: 'App',
 		components: {
-			MyGoals,
-			MonsterSlayer,
-			FriendsContact,
-			LearningResourcesApp,
-			Form,
-			SurvayResultsApp,
-			UsersTeamsApp,
+			// MyGoals,
+			// MonsterSlayer,
+			// FriendsContact,
+			// LearningResourcesApp,
+			// Form,
+			// SurvayResultsApp,
+			// UsersTeamsApp,
+			Animation,
 		},
 	};
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import {createApp} from 'vue';
 import './style.css';
 import App from './App.vue';
-import teamUsersRouter from './projects/users-and-teams/routes.js';
+// import teamUsersRouter from './projects/users-and-teams/routes.js';
 
 
 
-createApp(App).use(teamUsersRouter).mount('#app');
+createApp(App)
+// .use(teamUsersRouter)
+.mount('#app');
