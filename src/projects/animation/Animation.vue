129e6766ba9ee528d08c66fecd0bf0b0a63f729e
MRAlirad <m.r.alirad75@gmail.com>
2023-09-14 13:17:35 +0330

    animation evetns

--- src/projects/animation/Animation.vue
+++ src/projects/animation/Animation.vue
@@ -67,29 +67,29 @@
 			};
 		},
 		methods: {
-			beforeEnter(element){
-				console.log('beforeEnter')
-				console.log(element)
+			beforeEnter(element) {
+				console.log('beforeEnter');
+				console.log(element);
 			},
-			enter(element){
-				console.log('enter')
-				console.log(element)
+			enter(element) {
+				console.log('enter');
+				console.log(element);
 			},
-			afterEnter(element){
-				console.log('afterEnter')
-				console.log(element)
+			afterEnter(element) {
+				console.log('afterEnter');
+				console.log(element);
 			},
-			beforeLeave(element){
-				console.log('beforeLeave')
-				console.log(element)
+			beforeLeave(element) {
+				console.log('beforeLeave');
+				console.log(element);
 			},
-			leave(element){
-				console.log('leave')
-				console.log(element)
+			leave(element) {
+				console.log('leave');
+				console.log(element);
 			},
-			afterLeave(element){
-				console.log('afterLeave')
-				console.log(element)
+			afterLeave(element) {
+				console.log('afterLeave');
+				console.log(element);
 			},
 			showUsers() {
 				this.userIsVisible = true;
